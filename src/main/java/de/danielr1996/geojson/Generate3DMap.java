9c8f655a6fbfa3291b743e513cc2a19df8e1d6cc
Daniel Richter <danielrichter@posteo.de>
2019-02-21 00:01:52 +0100

    elevation

--- src/main/java/de/danielr1996/geojson/Generate3DMap.java
+++ src/main/java/de/danielr1996/geojson/Generate3DMap.java
@@ -10,19 +10,26 @@ import org.geojson.MultiPoint;
 import org.geojson.Polygon;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.stream.Collectors;
 
 public class Generate3DMap {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         FeatureCollection featureCollection = Util.readFeatureCollection(Generate3DMap.class.getResourceAsStream("/generated/FeatureCollection.geo.json"),"FeatureCollection");
         Feature wetterSteinGebirge = featureCollection.getFeatures().stream()
 //                .filter(feature -> feature.getProperty("name").equals("Wettersteingebirge"))
                 .findFirst().get();
 
 
-        MultiPoint raster = Polygons.raster.apply(10).apply((Polygon)wetterSteinGebirge.getGeometry());
-//        MultiPoint rasterWithHeight = new MultiPoint();
-//        raster.getCoordinates().stream().map(ElevationService::getElevations).forEach(rasterWithHeight::add);
-//        ElevationService.getElevations(raster.getCoordinates());
+        MultiPoint raster = Polygons.raster.apply(50).apply((Polygon)wetterSteinGebirge.getGeometry());
+
+        String csv = ElevationService.getElevations(raster.getCoordinates()).stream()
+                .map(coord -> String.format("%s, %s, %s", coord.getLongitude(), coord.getLatitude()
+                        , coord.getAltitude())).collect(Collectors.joining("\n"));
+
+        Files.write(Paths.get("C:/Users/Daniel/Desktop/wetterstein.csv"), csv.getBytes());
         Feature mu = new Feature();
         mu.setGeometry(raster);
         featureCollection.add(mu);
--- src/main/java/de/danielr1996/geojson/geojson/ElevationService.java
+++ src/main/java/de/danielr1996/geojson/geojson/ElevationService.java
@@ -4,9 +4,11 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
+import one.util.streamex.StreamEx;
 import org.geojson.LngLatAlt;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -26,17 +28,30 @@ public class ElevationService {
     }
 
     public static List<LngLatAlt> getElevations(List<LngLatAlt> coords) {
+        List<LngLatAlt> newList = new ArrayList<>();
+        int gesamtSize = coords.size();
+        int chunks = gesamtSize/10;
+        for (int i = 0; i < coords.size(); i += 10) {
+            System.out.printf("Chunk %d of %d\n", i, chunks);
+            int toIndex = i + 10;
+            if (toIndex > coords.size()) toIndex = coords.size() - 1;
+            newList.addAll(get10Elevations(coords.subList(i, toIndex)));
+        }
+        return newList;
+    }
+
+
+    private static List<LngLatAlt> get10Elevations(List<LngLatAlt> coords) {
         OkHttpClient client = new OkHttpClient();
 
-        String query = coords.stream().map(coord->String.format("(%s,%s),",coord.getLongitude(), coord.getLatitude())).collect(Collectors.joining(""));
-//        System.out.println(query);
-        Request request = new Request.Builder().url("https://elevation-api.io/api/elevation?points="+query).build();
-        try {
-            Response response = client.newCall(request).execute();
-            System.out.println(new ObjectMapper().readValue(response.body().string(), ElevationServiceResponse.class).elevations.stream()
-                    .map(coord->String.format("%s, %s, %s",coord.lat, coord.lon
-                    , coord.elevation)).collect(Collectors.joining("\n")));
-
+        String query = coords.stream().map(coord -> String.format("(%s,%s),", coord.getLongitude(), coord.getLatitude())).collect(Collectors.joining(""));
+        Request request = new Request.Builder().url("https://elevation-api.io/api/elevation?points=" + query).build();
+        try (Response response = client.newCall(request).execute()) {
+            ElevationServiceResponse elevation = new ObjectMapper().readValue(response.body().string(), ElevationServiceResponse.class);
+            return StreamEx.zip(coords, elevation.elevations, (coord, el) -> {
+                coord.setAltitude(el.elevation);
+                return coord;
+            }).collect(Collectors.toList());
         } catch (IOException e) {
             e.printStackTrace();
         }
